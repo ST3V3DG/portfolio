a7333700c20591e58196deaa066e98c1c4768827
ST3V3DG <rosycrystal024@gmail.com>
2025-12-21 02:36:23 +0100

    add @vercel/analytics and @vercel/speed-insights dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import SmoothScroll from "@/providers/smooth-scroll";
 
 export const metadata: Metadata = {
@@ -19,6 +21,8 @@ export default function RootLayout({
           className={`font-archivo antialiased`}
         >
           {children}
+          <SpeedInsights />
+          <Analytics />
         </body>
       </SmoothScroll>
     </html>
