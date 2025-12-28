ddc720c601aeb50d67c790762bfb9302ae25237c
ST3V3DG <rosycrystal024@gmail.com>
2025-12-28 18:41:19 +0100

    show projects captions on mobile device

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -37,8 +37,8 @@ export default function Projects() {
                 <div className="lg:col-span-2 lg:row-span-2">
                     <Link aria-label="eden cosmetics" className="group relative lg:grayscale lg:hover:grayscale-0 transition duration-300" target="_blank" href="https://eden-cosmetics.vercel.app/">
                         <Image alt="project image" className="size-full object-cover group-hover:scale-110 transition duration-300" height={1500} src="/images/mac-eden-cosmetics.png" width={1500} />
-                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black to-75% opacity-0 group-hover:opacity-50 transition duration- pointer-events-none"></div>
-                        <div className="absolute bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
+                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black lg:to-75% lg:opacity-0 lg:group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
+                        <div className="absolute bottom-0 left-0 right-0 lg:opacity-0 lg:group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
                             <h3 className="flex items-center gap-1"><span>Eden Cosmetics</span><ExternalLink className="size-3 stroke-4" /></h3>
                             <ul className="flex flex-col justify-between items-end font-clash-display">
                                 <li>Cosmetics</li>
@@ -51,8 +51,8 @@ export default function Projects() {
                 <div className="lg:col-span-2 lg:row-span-1">
                     <Link aria-label="crypto trade alpha" className="group relative lg:grayscale lg:hover:grayscale-0 transition duration-300" target="_blank" href="https://crypto-trade-alpha.vercel.app/">
                         <Image alt="project image" className="size-full object-cover group-hover:scale-110 transition duration-300" height={1500} src="/images/iphone-crypto-trade-alpha.png" width={1500} />
-                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black to-75% opacity-0 group-hover:opacity-50 transition duration- pointer-events-none"></div>
-                        <div className="absolute bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
+                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black lg:to-75% lg:opacity-0 lg:group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
+                        <div className="absolute bottom-0 left-0 right-0 lg:opacity-0 lg:group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
                             <h3 className="flex items-center gap-1"><span>Crypto Trade Alpha</span><ExternalLink className="size-3 stroke-4" /></h3>
                             <ul className="flex flex-col justify-between items-end font-clash-display">
                                 <li>Crypto</li>
@@ -65,8 +65,8 @@ export default function Projects() {
                 <div className="lg:col-span-2 lg:row-span-1">
                     <Link aria-label="fashion studio" className="group relative lg:grayscale lg:hover:grayscale-0 transition duration-300" target="_blank" href="https://fashionstudio.vercel.app/">
                         <Image alt="project image" className="size-full object-cover group-hover:scale-110 transition duration-300" height={1500} src="/images/mac-fashion-studio.png" width={1500} />
-                        <div className="absolute inset-0 bg-linear-to-b from-transparent to-black to-75% opacity-0 group-hover:opacity-50 transition duration- pointer-events-none"></div>
-                        <div className="absolute bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
+                        <div className="absolute inset-0 bg-linear-to-b from-transparent to-black lg:to-75% lg:opacity-0 lg:group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
+                        <div className="absolute bottom-0 left-0 right-0 lg:opacity-0 lg:group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
                             <h3 className="flex items-center gap-1"><span>Fashion Studio</span><ExternalLink className="size-3 stroke-4" /></h3>
                             <ul className="flex flex-col justify-between items-end font-clash-display">
                                 <li>Fashion</li>
@@ -79,8 +79,8 @@ export default function Projects() {
                 <div className="lg:col-span-3 lg:row-span-1">
                     <Link aria-label="odysia hotel" className="group relative lg:grayscale lg:hover:grayscale-0 transition duration-300" target="_blank" href="https://odysia-hotel.vercel.app/">
                         <Image alt="project image" className="size-full object-cover group-hover:scale-110 transition duration-300" height={1500} src="/images/mac-odysia.png" width={1500} />
-                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black to-75% opacity-0 group-hover:opacity-50 transition duration- pointer-events-none"></div>
-                        <div className="absolute bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
+                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black lg:to-75% lg:opacity-0 lg:group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
+                        <div className="absolute bottom-0 left-0 right-0 lg:opacity-0 lg:group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
                             <h3 className="flex items-center gap-1"><span>Odysia Hotel</span><ExternalLink className="size-3 stroke-4" /></h3>
                             <ul className="flex flex-col justify-between items-end font-clash-display">
                                 <li>Journey</li>
@@ -93,8 +93,8 @@ export default function Projects() {
                 <div className="lg:col-span-1 lg:row-span-1">
                     <Link aria-label="steve the developer" className="group relative lg:grayscale lg:hover:grayscale-0 transition duration-300" target="_blank" href="https://stevethedeveloper.vercel.app/">
                         <Image alt="project image" className="size-full object-cover group-hover:scale-110 transition duration-300" height={1500} src="/images/ipad-air-steve-the-developer.png" width={1500} />
-                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black to-75% opacity-0 group-hover:opacity-50 transition duration- pointer-events-none"></div>
-                        <div className="absolute bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
+                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black lg:to-75% lg:opacity-0 lg:group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
+                        <div className="absolute bottom-0 left-0 right-0 lg:opacity-0 lg:group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
                             <h3 className="flex items-center gap-1"><span>Steve.D</span><ExternalLink className="size-3 stroke-4" /></h3>
                             <ul className="flex flex-col justify-between items-end font-clash-display">
                                 <li>Developer</li>
@@ -107,8 +107,8 @@ export default function Projects() {
                 <div className="lg:col-span-2 lg:row-span-1">
                     <Link aria-label="mck medical care" className="group relative lg:grayscale lg:hover:grayscale-0 transition duration-300" target="_blank" href="https://mckmedicalcare.vercel.app/">
                         <Image alt="project image" className="size-full object-cover group-hover:scale-110 transition duration-300" height={1500} src="/images/iphone-mck-medical-care.png" width={1500} />
-                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black to-75% opacity-0 group-hover:opacity-50 transition duration- pointer-events-none"></div>
-                        <div className="absolute bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
+                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black lg:to-75% lg:opacity-0 lg:group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
+                        <div className="absolute bottom-0 left-0 right-0 lg:opacity-0 lg:group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
                             <h3 className="flex items-center gap-1"><span>MCK Medical Care</span><ExternalLink className="size-3 stroke-4" /></h3>
                             <ul className="flex flex-col justify-between items-end font-clash-display">
                                 <li>Medical</li>
@@ -121,8 +121,8 @@ export default function Projects() {
                 <div className="lg:col-span-2 lg:row-span-1">
                     <Link aria-label="optiride" className="group relative lg:grayscale lg:hover:grayscale-0 transition duration-300" target="_blank" href="https://optiride.vercel.app/">
                         <Image alt="project image" className="size-full object-cover group-hover:scale-110 transition duration-300" height={1500} src="/images/ipad-optiride.png" width={1500} />
-                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black to-75% opacity-0 group-hover:opacity-50 transition duration- pointer-events-none"></div>
-                        <div className="absolute bottom-0 left-0 right-0 opacity-0 group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
+                        <div className="absolute inset-0  bg-linear-to-b from-transparent to-black lg:to-75% lg:opacity-0 lg:group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
+                        <div className="absolute bottom-0 left-0 right-0 lg:opacity-0 lg:group-hover:opacity-100 text-accent transition duration-300 p-6 flex justify-between items-start">
                             <h3 className="flex items-center gap-1"><span>OptiRide</span><ExternalLink className="size-3 stroke-4" /></h3>
                             <ul className="flex flex-col justify-between items-end font-clash-display">
                                 <li>Carpooling</li>
